ef53e2173852d90350f2f7599aa6b8011e38d244
HKey <s06139@gmail.com>
2016-02-28 17:32:05 +0900

    Fix "Commentary" section

--- mozc-temp.el
+++ mozc-temp.el
@@ -23,7 +23,7 @@
 
 ;;; Commentary:
 
-;; mozc-temp is a mozc wrapper for modeless input of Japanese.
+;; mozc-temp is a wrapper of mozc for modeless input of Japanese.
 ;; This is inspired by ac-mozc.
 
 ;; Please see https://github.com/HKey/mozc-temp/blob/master/README.md
